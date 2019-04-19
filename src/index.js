d64a26fbd84b18b39adaa977a4672a9e919135d7
Alex Dubinchyk <alexs.dbk@gmail.com>
2019-04-19 08:20:42 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+const buffer = [''];
+let cur = 0;
 
+function bufferIn(str) {
+	buffer.push(buffer[cur] + str);
+    cur++;
+    return buffer[cur];
+}
+
+function ctrlZ() {
+  if (cur > 0) {
+	cur--;
+  }
+  return buffer[cur];
+}
+
+function ctrlY() {
+  if (cur < buffer.length) {
+	cur++;
+  }
+  return buffer[cur];
+}
+
+console.log('It should acc buffer abc', bufferIn('abc') === 'abc', buffer);
+console.log('It should acc buffer abc and defgt', bufferIn(' and defgt') === 'abc and defgt');
+console.log('It should acc buffer abc and defgt!', bufferIn('!') === 'abc and defgt!');
+
+console.log('It should ctrl + Z and return abc and defgt', ctrlZ() === 'abc and defgt');
+console.log('It should ctrl + Z and return abc and defgt', ctrlZ() === 'abc');
+console.log('It should ctrl + Z and return', ctrlZ() === '');
+console.log('It should ctrl + Z and return', ctrlZ() === '');
